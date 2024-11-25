f06676a716ea7a2a614b5aa2576b82e356071b1e
nithyasri200206 <nithyasricse02@gmail.com>
2024-11-25 14:17:34 +0530

    factorial

---
+++ EclipseGit/src/com/botree/Example.java
@@ -0,0 +1,16 @@
+package com.botree;
+
+public class Example {
+	public static void main(String[] args) {
+		
+		int i,fact=1;
+		int n=5;
+
+		for(i=1;i<=n;i++) {
+			fact=fact*i;
+		}
+
+		System.out.println(fact);
+	}
+
+}
